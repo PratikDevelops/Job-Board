e1e85acce8cb3cc950bffb6f2bf815af5f6670e1
PratikDevelops <kepratik8@gmail.com>
2024-12-26 19:15:08 +0530

    Fix Home.jsx import issue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import HeroSection from '../Components/HeroSection'
 import JobListing from '../Components/JobListing'
-import AppDownload from '../Components/AppDownload.jsX'
+
 import Footer from '../Components/Footer'
+import AppDownload from '../Components/AppDownload.jsx'
 
 function Home() {
   return (
